f942adbe1f93e90b749646665b403ff7fd78aff5
SorrowfulBlackout <iievliievdenis@gmail.com>
2021-02-27 21:13:06 +0200

    first task on course

---
+++ js/my.js
@@ -0,0 +1,20 @@
+'use strict';
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+let firstQuestion = prompt('Один из последних просмотренных фильмов?', ''),
+    secondQuestion = prompt('На сколько оцените его?', ''),
+    thirdQuestion = prompt('Один из последних просмотренных фильмов?', ''),
+    fourthQuestion = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies = firstQuestion + ":" + secondQuestion;
+personalMovieDB.movies = thirdQuestion + ":" + fourthQuestion;
+console.log(personalMovieDB);
+
