75dd154cb5f1831b39fa2add57d5d9995a66cb2b
SorrowfulBlackout <iievliievdenis@gmail.com>
2021-02-28 19:11:13 +0200

    second task on course

--- js/my.js
+++ js/my.js
@@ -9,12 +9,26 @@ let personalMovieDB = {
     privat : false
 };
 
-let firstQuestion = prompt('Один из последних просмотренных фильмов?', ''),
-    secondQuestion = prompt('На сколько оцените его?', ''),
-    thirdQuestion = prompt('Один из последних просмотренных фильмов?', ''),
-    fourthQuestion = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies = firstQuestion + ":" + secondQuestion;
-personalMovieDB.movies = thirdQuestion + ":" + fourthQuestion;
+for(let i = 0; i <= 1; i++){
+    let firstQuestion = prompt('Один из последних просмотренных фильмов?', ''),
+    secondQuestion = prompt('На сколько оцените его?', '');
+    if(firstQuestion && secondQuestion && 
+        firstQuestion.length && secondQuestion.length < 50 && 
+        firstQuestion && secondQuestion != ''){
+            personalMovieDB.movies = firstQuestion + ":" + secondQuestion;
+        } else {
+          i--;
+        }
+}
+if(personalMovieDB.count <= 10){
+        alert('Too many films watched');
+    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        alert('You are classic watcher');
+    } else if(personalMovieDB.count > 30){
+        alert('You are a film lovers!');
+    } else {
+        alert('Error');
+    }
 console.log(personalMovieDB);
+
 
